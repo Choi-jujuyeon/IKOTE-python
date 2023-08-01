064301a3d007adeb980f12bb0c9f433307a324b4
Choi-jujuyeon <chlwndus1216@naver.com>
2023-08-01 08:32:22 +0000

    list-datatype-initialize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # from number_datatype import integer
 # from number_datatype import realNum
 # from number_datatype import round
-from number_datatype import operator
+# from number_datatype import operator
+from list_datatype import list
